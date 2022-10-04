42ea74e4da8dcf8087b11ac9a5b7fbe69c97ee57
gempir <daniel.pasch.s@gmail.com>
2022-10-04 20:14:34 +0200

    Fix Jaxerie's bot website (#16)

--- app.py
+++ app.py
@@ -112,11 +112,6 @@ bots = [
             streamer=('linneafly', 'Linneafly'),
             ),
         TwitchBot(
-            website='https://jax.pajbot.com',
-            bot='Boterie',
-            streamer=('jaxerie', 'Jaxerie'),
-            ),
-        TwitchBot(
             website='https://nanonoko.pajbot.com',
             bot='NanoNoBot',
             streamer=('nanonoko', 'Nanonoko'),
